59e059bda65eb91b64272b367c7db6f7f12d52ec
Vladyslav Dmitriyev <vladyslav.dmitriyev@gmail.com>
2021-01-24 17:16:11 +0200

    add assert to add_remove_elements_page

--- src/tests/test_add_remove_elements_page.py
+++ src/tests/test_add_remove_elements_page.py
@@ -5,8 +5,8 @@ def test_add_remove_elements_page(driver):
     add_remove_elements_page.visit_add_remove_elements_page()
     assert add_remove_elements_page.is_add_remove_elements_page()
     assert add_remove_elements_page.is_add_element_button()
-    elements_count = 50
-    add_remove_elements_page.add_element(elements_count)
-    assert add_remove_elements_page.count_delete_buttons() == elements_count
-    add_remove_elements_page.delete_elements()
-    assert add_remove_elements_page.count_delete_buttons() == 0
+    add_remove_elements_page.add_element(50)
+    assert add_remove_elements_page.count_delete_buttons() == 50
+    add_remove_elements_page.delete_elements(25)
+    add_remove_elements_page.add_element(5)
+    assert add_remove_elements_page.count_delete_buttons() == 30
